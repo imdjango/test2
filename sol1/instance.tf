e12247a75fae0f9d7d3198473b4430ba40b1d1fa
imdjango <63759249+imdjango@users.noreply.github.com>
2020-05-11 11:33:44 +0900

    Update instance.tf

--- sol1/instance.tf
+++ sol1/instance.tf
@@ -1,10 +1,20 @@
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  key_name      = aws_key_pair.mykey.key_name
+  connection {
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
-}
-output "ip" {
-  value = aws_instance.example.public_ip
+  tags = {
+    Name = "My Instance"
+  }
 }
